5e428eb0139dc0e428606868aff0f23bfbcd1936
Denis <Dysprosium92@ya.ru>
2021-05-21 19:49:54 +0300

    Added simple interface for formater

---
+++ gendiff/formaters/formater.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+
+def formats():
+    """Return available formats.
+
+    Returns:
+        formats (list of string): available formats
+    """
+    return ['json', 'plain']
+
+
+def default_format():
+    """Return type default format.
+
+    Returns:
+        result (str): type default format
+    """
+    return 'plain'
