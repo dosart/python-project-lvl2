d47247d5e9f1758c657db23463542a0658cac920
Denis <Dysprosium92@ya.ru>
2021-05-30 10:38:37 +0300

    Refactoring loader.py.

--- gendiff/loader.py
+++ gendiff/loader.py
@@ -6,29 +6,11 @@ import json
 import yaml
 import os
 
-from yaml import loader
-
-
-class GendiffFileError(Exception):
-    pass  # noqa: WPS420, WPS604
-
-
-def invalid_extension(file_name):
-    return "{0} has invalid file extension. nuse one of: .json .yaml .yml".format(
-        file_name
-    )
-
-
-def _file_open_error(file_name):
-    return "{0}: file open error".format(file_name)
-
-
-def _incorrect_yaml_file(file_name):
-    return "{0}: incorrect YAML file".format(file_name)
-
-
-def _incorrect_json_file(file_name):
-    return "{0}: incorrect JSON file".format(file_name)
+from gendiff.exceptions.GendiffFileError import GendiffFileError
+from gendiff.exceptions.GendiffFileError import invalid_extension
+from gendiff.exceptions.GendiffFileError import file_open_error
+from gendiff.exceptions.GendiffFileError import incorrect_yaml_file
+from gendiff.exceptions.GendiffFileError import incorrect_json_file
 
 
 def _switch_loader(loader_name):
@@ -73,10 +55,10 @@ def load_data(file_path):
         with open(file_path, "r") as input_file:
             loader(input_file)
     except OSError:
-        raise GendiffFileError(_file_open_error(file_path))
+        raise GendiffFileError(file_open_error(file_path))
     except json.JSONDecodeError:
-        raise GendiffFileError(_incorrect_yaml_file(file_path))
+        raise GendiffFileError(incorrect_yaml_file(file_path))
     except yaml.YAMLError:
-        raise GendiffFileError(_incorrect_json_file(file_path))
+        raise GendiffFileError(incorrect_json_file(file_path))
     except GendiffFileError:
         raise
